0ed55464296127c933ea0dab8aa290a286809c84
Arink Verma <arinkverma@gmail.com>
2021-02-26 04:48:01 +0530

    Update image

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,8 @@
 FROM python:3.10.0a5-alpine3.13
-
-RUN pip install rq==1.7.0
-RUN pip install flask==1.1.2
-RUN pip install rq_dashboard==0.6.1
-RUN pip install Flask-RQ2==18.3
-
+ENV MAGICK_HOME=/usr
+RUN apk add --no-cache imagemagick && apk add --no-cache imagemagick-dev
 ADD vernacular_image vernacular_image
-#WORKDIR /vernacular_image
+RUN pip install -r /vernacular_image/requirements.txt
 ENV PYTHONPATH "${PYTHONPATH}:/vernacular_image"
 RUN chmod +x /vernacular_image/start.sh
 
--- vernacular_image/workers/image_processor.py
+++ vernacular_image/workers/image_processor.py
@@ -1,8 +1,15 @@
-import time
+from wand.image import Image
+from wand.display import display
 
 class ImageProcessor(object):
 
     @classmethod
     def create_banner(cls, x, y):
-        time.sleep(120)
+        with Image(filename='/vernacular_image/workers/lisa.jpg') as img:
+            print(img.size)
+            for r in 1, 2, 3:
+                with img.clone() as i:
+                    i.resize(int(i.width * r * 0.25), int(i.height * r * 0.25))
+                    i.rotate(90 * r)
+                    i.save(filename='lisa-{0}.jpg'.format(r))
         return x+y
